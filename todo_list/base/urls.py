010e0acd20d44fd3073df5807ed7afbd64e40ac7
Roman-24 <xbitarovsky@stuba.sk>
2021-06-20 17:46:13 +0200

    task delete button
    pridanie možnosti vymazania tasku

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreat, TaskUpdate
+from .views import TaskList, TaskDetail, TaskCreat, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create', TaskCreat.as_view(), name='task-create'),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
 ]
